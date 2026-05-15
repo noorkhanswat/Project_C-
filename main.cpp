a23ad24ea8ceae25235f517cf144b6fa2bfd6cce
noorkhanswat <noorkhan.na2012@gmail.com>
2026-05-15 05:10:07 +0400

    draw heart code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
  void printHelloWorld() {
      cout << "Hello, World!" << endl;
  }
+
+ // function to print a simple ASCII art of a heart using asterisks 
+    void drawHeart() {
+        for (float y = 1.5f; y > -1.5f; y -= 0.1f) {
+            for (float x = -1.5f; x < 1.5f; x += 0.05f) {
+                float a = x * x + y * y - 1;
+                if (a * a * a - x * x * y * y * y <= 0.0f) {
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
  // function to draw heart shape using asterisks
     void drawHeart() {
         for (float y = 1.5f; y > -1.5f; y -= 0.1f) {
