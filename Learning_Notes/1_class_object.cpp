345417d4a439b2249496f340961cc0f34e2e1ada
noorkhanswat <noorkhan.na2012@gmail.com>
2026-05-15 16:20:01 +0400

    write code for class and member of class

---
+++ Learning_Notes/1_class_object.cpp
@@ -0,0 +1,16 @@
+// classess and objects
+#include <iostream>
+using namespace std;
+// class and object with detail comments, and function 
+//Data members > attributes of class
+//Member functions > behavior of class
+
+class student {
+
+private:
+    char* name;
+    int roll_no;
+public:
+        void setName(char* name); // Behavior of class
+        void setRoll_no(int newRollNo); // Behavior of class 
+    };
