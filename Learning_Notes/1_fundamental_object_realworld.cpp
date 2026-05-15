e16431bc4ae1cbf2e43cd66d52e59e282cabdf2c
noorkhanswat <noorkhan.na2012@gmail.com>
2026-05-15 18:18:24 +0400

    save file

--- Learning_Notes/1_fundamental_object_realworld.cpp
+++ Learning_Notes/1_fundamental_object_realworld.cpp
@@ -26,9 +26,9 @@ int main() {
     // calling the member function to display the details of the car
     myCar.displayDetails();
     return 0;
-};
+    };
 /*
 In this code, we have defined a class named 'Car' that represents a real world object, which is a car. The class has three member variables: 'brand', 'model', and 'year', which represent the properties of a car. The class also has a member function named 'displayDetails' that displays the details of the car on the console.
 In the 'main' function, we create an object of the 'Car' class named 'myCar'. We then set the properties of the car by assigning values to the member variables. Finally, we call the member function '             
 displayDetails' to display the details of the car on the console. This code demonstrates how we can model real world objects using classes in C++. Each object of the class can have its own unique properties and behaviors, which allows us to represent different instances of the same type of object.
-*/  
+*/ 
